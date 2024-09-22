31d22bf3e26eee28c47544ec6a429446ae018bae
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-09-22 17:40:40 +0900

    layout作成

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>book-app</title>
-</head>
-<body>
+<x-layout>
   <h1>本一覧</h1>
   <p>
     <a href="{{ url('/') }}">メニューへ戻る</a>
@@ -14,5 +6,4 @@
   <p>
     <a href="{{ url('/book_new') }}">新規登録</a>
   </p>
-</body>
-</html>
+</x-layout>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>book-app</title>
-</head>
-<body>
+<x-layout>
   <h1>メニュー</h1>
   <a href="{{ url('/books') }}">本一覧</a>
-</body>
-</html>
+</x-layout>
