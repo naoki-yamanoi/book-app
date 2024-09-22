980ea6fef82a7363b088cec4fa40bee9a18b0269
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-09-22 17:28:56 +0900

    基本画面ルート設定など

---
+++ resources/views/books.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>book-app</title>
+</head>
+<body>
+  <h1>本一覧</h1>
+  <p>
+    <a href="{{ url('/') }}">メニューへ戻る</a>
+  </p>
+  <p>
+    <a href="{{ url('/book_new') }}">新規登録</a>
+  </p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+// メニュー
+Route::get('/', function () {
+    return view('index');
+});
+
+// 本一覧
+Route::get('/books', function () {
+    return view('books');
+});
+
+// 本登録ページ
+Route::get('/book_new', function () {
+    return view('book_new');
+});
+
+// 本登録
+Route::post('/book_new', function () {
+    return view('books');
+});
+
+// 本詳細ページ
+Route::get('/book', function () {
+    return view('books');
+});
+
+// 本更新
+Route::put('/book', function () {
+    return view('books');
+});
+
+// 本削除
+Route::delete('/book', function () {
+    return view('books');
 });
